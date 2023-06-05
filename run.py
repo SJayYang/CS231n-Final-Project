b7b57984090f8606f6c5b54fd6d55a542534c6e8
Ubuntu <ubuntu@ip-172-31-10-93.us-west-2.compute.internal>
2023-06-05 22:04:52 +0000

    Created batch_size creation of new images

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from torchvision import transforms
 from torchvision.utils import save_image
 from tqdm import tqdm
 from diffusers import StableDiffusionPipeline
+from itertools import chain
 
 
 # Step 1: Define the output directory
@@ -31,7 +32,8 @@ def save_images(generated_images, disease_type):
     for i, generated_image in enumerate(tqdm(generated_images)):
 
         # Save the image
-        image_name = f'{disease_type}_{i}.{file_format.lower()}'
+        num = i + 64
+        image_name = f'{disease_type}_{num}.{file_format.lower()}'
         output_path = os.path.join(output_dir, image_name)
         generated_image.save(output_path, format=file_format, optimize=True, quality=compression_level, **image_options)
 
@@ -44,11 +46,22 @@ diseases = [ 'Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 
                 'Pneumothorax', 'Consolidation', 'Edema', 'Emphysema', 'Fibrosis', 'Pleural_Thickening', 'Hernia']
 
 print("Start inference loop")
+num_examples = 256
+batch_size = 8
+def split_list_into_batches(lst, batch_size):
+    return [lst[i:i+batch_size] for i in range(0, len(lst), batch_size)]
+
 for disease in diseases: 
-    prompts =  generate_dataset_prompt(disease, num_data = 10)
-    output = pipe(prompts, num_inference_steps=75, height=512, width=512, guidance_scale=4)
-    generated_images = output.images
-    save_images(generated_images=generated_images, disease_type=disease)
+    print(disease)
+    total_images = []
+    prompts =  generate_dataset_prompt(disease, num_data = num_examples)
+    prompts_split = split_list_into_batches(prompts, batch_size=batch_size)
+    for prompt_batch in tqdm(prompts_split):
+        output = pipe(prompt_batch, num_inference_steps=75, height=512, width=512, guidance_scale=4)
+        generated_images_batch = output.images
+        total_images.append(generated_images_batch)
+    total_images = list(chain.from_iterable(total_images))
+    save_images(generated_images=total_images, disease_type=disease)
 
 
 
